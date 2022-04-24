2b475679501f16a4a501c5877708495702c5d33e
KoreanShortHair <103630274+korshorthaircat@users.noreply.github.com>
2022-04-24 20:28:03 +0900

    Update Ex11_01.java
    주석 추가, 몇 가지 메서드 추가

--- Ch11/src/Ex11_01.java
+++ Ch11/src/Ex11_01.java
@@ -5,20 +5,24 @@ import java.util.*;
 class Ex11_01 {
 	public static void main(String[] args) {
 		
-		ArrayList list1 = new ArrayList(10); //길이가 10인 ArrayList 생성
+		ArrayList list1 = new ArrayList(10); //기본 길이(용량, capacity)가 10인 ArrayList를 생성
 		//add(Object o) 
-		list1.add(new Integer(5));
-		list1.add(4); //오토박싱에 의해 기본형이 참조형으로 자동 변환됨
+		list1.add(new Integer(5)); //ArrayList에는 객체만 저장 가능 
+		list1.add(4); //이와 같이 쓸 경우 컴파일러의 오토박싱에 의해 기본형이 참조형으로 자동 변환됨
 		list1.add(new Integer(2));
 		list1.add(new Integer(0));
 		list1.add(new Integer(1));
 		list1.add(new Integer(3));
 		
-
-		ArrayList list2 = new ArrayList(list1.subList(1,4)); //List subList(int fromIdx, int toIdx) - fromIdx부터 toIdx까지(toIdx는 포함X) 저장된 객체를 반환 
-		print(list1, list2);
+		//List subList(int fromIdx, int toIdx) - fromIdx부터 toIdx까지(toIdx는 포함X) 저장된 객체를 반환 
+		ArrayList list2 = new ArrayList(list1.subList(1,4)); // 1<=index<4
+		//위의 한 줄을 풀어쓰면 다음과 같다.
+		//List sub = list1.subList(1,4); //subList()는 읽기만 가능
+		//ArrayList list2 = new ArrayList(sub); //sub와 같은 내용의 ArrayList 생성
+		print(list1, list2); //편한 출력을 위해 만든 메서드
 		
-		//Collections.sort(List l) - 정렬 
+		//Collections.sort(List l) - 오름차순으로 정렬 
+		//참고) Collection은 인터페이스, Collections는 유틸 클래스
 		Collections.sort(list1);
 		Collections.sort(list2);
 		print(list1, list2);
@@ -31,7 +35,20 @@ class Ex11_01 {
 		list2.add(3, "A"); // add(int idx, Collection c) - 지정된 위치 idx에 객체 저장 
 		print(list1, list2);
 		
-		list2.set(3, "AA"); // set(int idx, Collection c) -지정된 위치 idx에 객체 저장(치환)
+		list2.set(3, "AA"); // set(int idx, Collection c) -지정된 위치 idx에 객체 저장(치환, 변경)
+		print(list1, list2);
+		
+		
+		list1.add(0, "1"); //지정된 위치 0에 문자열 "1"을 저장한다.
+		//int indexOf(Object o) - 지정된 객체가 어느 위치(index)에 있는지 찾아 반환(못 찾으면 -1 반환)
+		System.out.println("indedx= " + list1.indexOf("1"));
+		System.out.println("indedx= " + list1.indexOf(new Integer(1)));
+		print(list1, list2);
+		
+		//boolean remove(Object o) - 지정한 객체를 삭제함
+		list1.remove(new Integer(1));
+		//Object remove(int index) - 지정된 위치(index)에 있는 객체를 삭제함
+		list1.remove(1);
 		print(list1, list2);
 		
 		//boolean retainAll()
